f1f45868c31082b0fcafa2abc8d4e64fe65b3791
mathewsmb <31582465+mathewsmb@users.noreply.github.com>
2017-11-21 12:22:06 -0500

    system

---
+++ WebSystemRest/WebSystemRest/Controllers/MantenimientoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSystemRest.Controllers
+{
+    public class MantenimientoController : Controller
+    {
+        // GET: Mantenimiento
+        public ActionResult Ingredientes()
+        {
+            return View();
+        }
+
+        public ActionResult Menus()
+        {
+            return View();
+        }
+
+        public ActionResult Platos()
+        {
+            return View();
+        }
+
+        public ActionResult Recetas()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebSystemRest/WebSystemRest/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSystemRest.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Usuario()
+        {
+            return View();
+        }
+        public ActionResult Contraseña()
+        {
+            return View();
+        }
+    }
+}
